9caa6e99556547ab801e9f94fa1f0f50e0703c62
RiaKang <43290484+RiaKang@users.noreply.github.com>
2023-05-12 16:10:08 +0900

    Update server_dns.py
    import socket -> from socket import *
    나중에 dns 파일 쓰기 위해서 import 해두었습니다

--- Socket/DNS/server_dns.py
+++ Socket/DNS/server_dns.py
@@ -1,4 +1,5 @@
-import socket
+from socket import *
+from dns import *
 
 host = ''
 port = 12000
@@ -7,18 +8,21 @@ def to_ascii(msg):
     return str([ord(c) for c in msg]).encode('utf-8')
 
 if __name__ == "__main__":   
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    with socket(AF_INET, SOCK_STREAM) as s:
         s.bind((host, port))
         s.listen()
 
         print(f'Server listening on port {port}')
+        
         while True:
             conn, addr = s.accept()
             print(f'Connected by {addr}')
+
             with conn:  # context manager to ensure exit
                 while True:                
                     data = conn.recv(1024)
                     msg = data.decode('utf-8')
                     print(f"Received from {addr}: {msg}")
+                    #client에서 받은 입력을 분석하여 각 행동을 구분하는 라인 제작
                     conn.sendall(to_ascii(msg))
                     break
